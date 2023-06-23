5636c793cc0e3627c2d657dfdc1f9d21b0da9344
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-23 06:02:20 +0600

    display list of countries in th html page

--- src/main/java/com/yessenali/fleetapp/controllers/CountryController.java
+++ src/main/java/com/yessenali/fleetapp/controllers/CountryController.java
@@ -1,12 +1,24 @@
 package com.yessenali.fleetapp.controllers;
 
+import com.yessenali.fleetapp.models.Country;
+import com.yessenali.fleetapp.services.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
     @GetMapping("/countries")
-    public String getCountries(){
+    public String getCountries(Model model){
+        List<Country> countryList = countryService.getCountries();
+        model.addAttribute("countries", countryList);
         return "Country";
     }
 }
--- src/main/java/com/yessenali/fleetapp/services/CountryService.java
+++ src/main/java/com/yessenali/fleetapp/services/CountryService.java
@@ -1,7 +1,20 @@
 package com.yessenali.fleetapp.services;
 
+import com.yessenali.fleetapp.models.Country;
+import com.yessenali.fleetapp.repositories.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    //return list of countries;
+    public List<Country> getCountries(){
+        return countryRepository.findAll();
+    }
 }
