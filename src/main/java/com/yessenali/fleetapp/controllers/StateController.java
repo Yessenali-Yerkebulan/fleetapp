15e27c175f63c283e21d2d4239f8c6d3fb94a3fb
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-24 15:37:42 +0600

    add code to StateController and StateService

--- src/main/java/com/yessenali/fleetapp/controllers/StateController.java
+++ src/main/java/com/yessenali/fleetapp/controllers/StateController.java
@@ -1,12 +1,49 @@
 package com.yessenali.fleetapp.controllers;
 
+
+import com.yessenali.fleetapp.models.State;
+import com.yessenali.fleetapp.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class StateController {
+    @Autowired
+    private StateService stateService;
+
     @GetMapping("/states")
-    public String getStates(){
+    public String getStates(Model model){
+        List<State> countryList = stateService.getStates();
+        model.addAttribute("countries", countryList);
         return "State";
+    }
+
+    @PostMapping("/states/new")
+    public String addNew(State state){
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+    @RequestMapping("/states/findById/")
+    @ResponseBody
+    public Optional<State> findById(int id){
+        return stateService.findById(id);
+    }
+
+    @RequestMapping(value = "/states/update",  method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(State state){
+        stateService.save(state);
+        return "redirect:/states";
+    }
+
+    @RequestMapping(value = "/states/delete",  method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id){
+        stateService.delete(id);
+        return "redirect:/states";
     }
 }
--- src/main/java/com/yessenali/fleetapp/services/StateService.java
+++ src/main/java/com/yessenali/fleetapp/services/StateService.java
@@ -1,7 +1,34 @@
 package com.yessenali.fleetapp.services;
 
+import com.yessenali.fleetapp.models.State;
+import com.yessenali.fleetapp.repositories.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StateService {
+    @Autowired
+    private StateRepository stateRepository;
+
+    //return list of states;
+    public List<State> getStates(){
+        return stateRepository.findAll();
+    }
+
+    //save new state
+    public void save(State country){
+        stateRepository.save(country);
+    }
+
+    //get country by id
+    public Optional<State> findById(int id){
+        return stateRepository.findById(id);
+    }
+
+    public void delete(Integer id) {
+        stateRepository.deleteById(id);
+    }
 }
