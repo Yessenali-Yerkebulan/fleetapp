35fd0bfbf32f2a90ca4a26604ba105d4b455f1c6
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-26 15:51:21 +0600

    setup all the Common Objects and their Pages

--- src/main/java/com/yessenali/fleetapp/controllers/VehicleMaintenanceController.java
+++ src/main/java/com/yessenali/fleetapp/controllers/VehicleMaintenanceController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleMaintenanceController {
-    @GetMapping("/vehiclemaintenances")
+    @GetMapping("/vehicleMaintenances")
     public String getVehicleMaintenances(){
         return "VehicleMaintenance";
     }
