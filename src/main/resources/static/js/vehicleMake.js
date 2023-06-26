f5e6e211f5884b08d4121a9042fa147b5e2de117
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-26 11:06:17 +0600

    add JavaScript

---
+++ src/main/resources/static/js/vehicleMake.js
@@ -0,0 +1,31 @@
+$('document').ready(function() {
+    $('table #editButton').on('click', function(event){
+        event.preventDefault();
+        let href = $(this).attr('href');
+        $.get(href, function (location, status){
+            $('#idEdit').val(location.id);
+            $('#descriptionEdit').val(location.description);
+            $('#detailsEdit').val(location.details);
+        });
+        $('#editModal').modal();
+    });
+    $('table #detailsButton').on('click',function(event) {
+        event.preventDefault();
+        let href= $(this).attr('href');
+        $.get(href, function(location, status){
+            $('#idDetails').val(location.id);
+            $('#descriptionDetails').val(location.description);
+            $('#detailsDetails').val(location.details);
+            $('#lastModifiedByDetails').val(location.lastModifiedBy);
+            $('#lastModifiedDateDetails').val(location.lastModifiedDate.substr(0,19).replace("T", " "));
+        });
+        $('#detailsModal').modal();
+    });
+
+    $('table #deleteButton').on('click',function(event) {
+        event.preventDefault();
+        let href = $(this).attr('href');
+        $('#confirmDeleteButton').attr('href', href);
+        $('#deleteModal').modal();
+    });
+});
