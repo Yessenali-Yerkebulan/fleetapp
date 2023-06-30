76d8f4090ff30499aace061db336bdbe23c7751f
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-06-30 22:48:30 +0600

    Select, Insert, Update and Delete Vehicle Movements

--- src/main/java/com/yessenali/fleetapp/controllers/VehicleMovementController.java
+++ src/main/java/com/yessenali/fleetapp/controllers/VehicleMovementController.java
@@ -1,12 +1,56 @@
 package com.yessenali.fleetapp.controllers;
 
+import com.yessenali.fleetapp.models.VehicleMovement;
+import com.yessenali.fleetapp.services.LocationService;
+import com.yessenali.fleetapp.services.SupplierService;
+import com.yessenali.fleetapp.services.VehicleMovementService;
+import com.yessenali.fleetapp.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class VehicleMovementController {
-    @GetMapping("/vehiclemovements")
-    public String getVehicleMovements(){
+    @Autowired
+    private VehicleMovementService vehicleMovementService;
+    @Autowired
+    private VehicleService vehicleService;
+    @Autowired
+    private LocationService locationService;
+    @GetMapping("/vehicleMovements")
+    public String getStates(Model model){
+        List<VehicleMovement> vehicleMovementList = vehicleMovementService.getVehicleMovements();
+        model.addAttribute("vehicleMovements", vehicleMovementList);
+        model.addAttribute("vehicles", vehicleService.getVehicles());
+        model.addAttribute("locations", locationService.getLocations());
         return "VehicleMovement";
+    }
+
+    @PostMapping("/vehicleMovements/new")
+    public String addNew(VehicleMovement vehicleMovement){
+        vehicleMovementService.save(vehicleMovement);
+        return "redirect:/vehicleMovements";
+    }
+
+    @RequestMapping("/vehicleMovements/findById/")
+    @ResponseBody
+    public Optional<VehicleMovement> findById(int id){
+        return vehicleMovementService.findById(id);
+    }
+
+    @RequestMapping(value = "/vehicleMovements/update",  method = {RequestMethod.PUT, RequestMethod.GET})
+    public String update(VehicleMovement vehicleMovement){
+        vehicleMovementService.save(vehicleMovement);
+        return "redirect:/vehicleMovements";
+    }
+
+    @RequestMapping(value = "/vehicleMovements/delete",  method = {RequestMethod.DELETE, RequestMethod.GET})
+    public String delete(Integer id){
+        vehicleMovementService.delete(id);
+        return "redirect:/vehicleMovements";
     }
 }
--- src/main/java/com/yessenali/fleetapp/services/VehicleMovementService.java
+++ src/main/java/com/yessenali/fleetapp/services/VehicleMovementService.java
@@ -1,7 +1,34 @@
 package com.yessenali.fleetapp.services;
 
+import com.yessenali.fleetapp.models.VehicleMovement;
+import com.yessenali.fleetapp.repositories.VehicleMovementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleMovementService {
+    @Autowired
+    private VehicleMovementRepository vehicleMovementRepository;
+
+    //return list of vehicleMovements;
+    public List<VehicleMovement> getVehicleMovements(){
+        return vehicleMovementRepository.findAll();
+    }
+
+    //save new vehicleMovement
+    public void save(VehicleMovement vehicleMovement){
+        vehicleMovementRepository.save(vehicleMovement);
+    }
+
+    //get vehicleMovement by id
+    public Optional<VehicleMovement> findById(int id){
+        return vehicleMovementRepository.findById(id);
+    }
+
+    public void delete(Integer id) {
+        vehicleMovementRepository.deleteById(id);
+    }
 }
